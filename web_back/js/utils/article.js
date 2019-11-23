6cbe108aa8d9b0fe7ea8709d189629d9bbbfd334
Your Name <f137000360@qq.com>
2019-11-23 22:10:06 +0800

    筛选功能完成

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -3,7 +3,8 @@ var article={
     show:function(obj,callback){
 //传入obj参数
         $.get(APIURLS.article_show,{'page':obj.page,
-                                    'type':obj.type ,    },function(res){
+                                    'type':obj.type ,  
+                                    'state':obj.state,  },function(res){
 
 
             
